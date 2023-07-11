377c87f7cdb3e9e1e25cdbce2d6d0de0145aa478
AlexJamisone <business.homeit@gmail.com>
2023-07-11 21:42:30 +0300

    make custom scroll bar

--- src/UI/Admin/AdminProducts/AdminCreateProduct/SelectSize.tsx
+++ src/UI/Admin/AdminProducts/AdminCreateProduct/SelectSize.tsx
@@ -4,12 +4,11 @@ import SizeButton from './SizeButton';
 
 const SelectSize = () => {
 	const { data: sizes } = api.size.get.useQuery();
-	if (!sizes) return null;
 	return (
 		<>
 			<FormLabel>Выбери размеры</FormLabel>
-			<Stack direction="row">
-				{sizes.map((size) => (
+			<Stack direction="row" justifyContent="center" flexWrap="wrap">
+				{sizes?.map((size) => (
 					<SizeButton key={size.id} size={size} />
 				))}
 			</Stack>
--- src/UI/Admin/AdminProducts/AdminCreateProduct/index.tsx
+++ src/UI/Admin/AdminProducts/AdminCreateProduct/index.tsx
@@ -44,7 +44,30 @@ const AdminCreateProduct = () => {
 		}
 	};
 	return (
-		<Stack>
+		<Stack
+			gap={3}
+			maxW={630}
+			maxH={630}
+			overflowY="auto"
+			pr={3}
+			sx={{
+				'::-webkit-scrollbar': {
+					width: '5px',
+					height: '5px',
+				},
+				'::-webkit-scrollbar-track': {
+					bgColor: '#eee',
+					boxShadow:
+						'0 0 1px 1px #bbb, inset 0 0 7px rgba(0,0,0,0.3)',
+					borderRadius: '2xl',
+				},
+				'::-webkit-scrollbar-thumb': {
+					bgColor: 'linear-gradient(left, #96A6BF, #63738C)',
+					rounded: '2xl',
+					boxShadow: 'inset 0 0 1px 1px #5C6670',
+				},
+			}}
+		>
 			<Stack>
 				{productCreate(state.product).map(
 					({ label, name, placeholder, value, textarea }) => (
