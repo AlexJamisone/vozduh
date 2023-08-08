5a8a62e70880889e657efc5b9629a4c958c540c2
AlexJamisone <business.homeit@gmail.com>
2023-08-08 18:08:14 +0300

    make SEO optimization tag in product dirails

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,4 +1,5 @@
 import { Center } from '@chakra-ui/react';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { useReducer } from 'react';
 import ProductDitails from '~/UI/Product/ProductDitails';
@@ -16,8 +17,23 @@ const ProductDitailsPage = () => {
 	});
 	const [state, dispatch] = useReducer(productDitailsReducer, initial);
 	if (!product) return null;
+	const description = product.description.join(' ');
 	return (
 		<Center pt={[100, 200]}>
+			<Head>
+				<title>{product.name}</title>
+				<meta name="description" content={description} />
+				<meta
+					name="keywords"
+					content="украшения из серебра, серебряные украшения, ювелирные изделия, серебро, украшения ручной работы, подарки из серебра, воздух, vozduh, кольца из серебра, серьги из серебра, ожерелья из серебра, браслеты из серебра"
+				/>
+				<meta property="og:title" content={product.name} />
+				<meta property="og:description" content={description} />
+				<meta
+					property="og:image"
+					content={`https://utfs.io/f/${product.image[0] as string}`}
+				/>
+			</Head>
 			<ProductDitalsContext.Provider value={{ product, dispatch, state }}>
 				<ProductDitails
 					info={<ProductDitails.Info />}
