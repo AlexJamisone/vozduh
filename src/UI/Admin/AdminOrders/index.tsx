d2436db0416094149114be607fa1cd005db83ea3
AlexJamisone <business.homeit@gmail.com>
2023-08-14 16:11:56 +0300

    Finish InfinitScroll functionality

--- src/UI/Admin/AdminOrders/index.tsx
+++ src/UI/Admin/AdminOrders/index.tsx
@@ -1,32 +1,53 @@
-import { Button, Center } from '@chakra-ui/react';
-import { useState } from 'react';
+import { Center, Spinner, Stack } from '@chakra-ui/react';
+import React from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import { api } from '~/utils/api';
 import AdminOrderCard from './AdminOrderCard';
 
 const AdminOrders = () => {
-	const [page, setPage] = useState(0);
-	const { data, fetchNextPage } = api.order.getForAdmin.useInfiniteQuery(
-		{
-			limit: 5,
-		},
-		{
-			getNextPageParam: (lastPage) => lastPage.nextCursor,
-		}
-	);
-	const handlFetchNextPage = async () => {
-		await fetchNextPage();
-		setPage((prev) => prev + 1);
-	};
-	const toShow = data?.pages[page]?.items;
+	const { data, fetchNextPage, hasNextPage } =
+		api.order.getForAdmin.useInfiniteQuery(
+			{
+				limit: 10,
+			},
+			{
+				getNextPageParam: (lastPage) => lastPage.nextCursor,
+			}
+		);
+	if (!data) return null;
 	return (
-		<Center gap={5} mx={5} flexWrap="wrap">
-			{toShow?.map((order) => (
-				<AdminOrderCard key={order.id} order={order} />
-			))}
-			<Button onClick={() => void handlFetchNextPage()}>
-				Показать ещё 4
-			</Button>
-		</Center>
+		<InfiniteScroll
+			dataLength={data.pages.reduce(
+				(total, page) => total + page.items.length,
+				0
+			)}
+			hasMore={hasNextPage ?? true}
+			loader={
+				<Center>
+					<Spinner />
+				</Center>
+			}
+			next={fetchNextPage}
+			style={{
+				overflow: 'hidden',
+			}}
+		>
+			<Stack
+				direction="row"
+				flexWrap="wrap"
+				gap={7}
+				justifyContent="center"
+				overflowY="hidden"
+			>
+				{data.pages.map((page, index) => (
+					<React.Fragment key={index}>
+						{page.items.map((order) => (
+							<AdminOrderCard order={order} key={order.id} />
+						))}
+					</React.Fragment>
+				))}
+			</Stack>
+		</InfiniteScroll>
 	);
 };
 
