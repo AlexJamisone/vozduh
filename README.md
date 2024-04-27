639c766f514ec45c6bf6a1ada7ecb168449aefbb
Alex Jamison <business.homeit@gmail.com>
2024-04-27 16:52:13 +0300

    make server side role

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 -   [x] Comment when auth in new-order
 -   [x] Deselect card PVZ and choose point again
--   [] make in user order tabel product stuff
+-   [x] make in user order tabel product stuff
--- src/pages/admin/[path].tsx
+++ src/pages/admin/[path].tsx
@@ -1,11 +1,28 @@
 import { Center } from '@chakra-ui/react';
+import { getAuth } from '@clerk/nextjs/server';
+import { Role } from '@prisma/client';
+import type { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import AdminOrders from '~/UI/Admin/AdminOrders';
 import AdminProducts from '~/UI/Admin/AdminProducts';
-import { api } from '~/utils/api';
-const AdminRouter = () => {
-	const { data: role } = api.user.getRole.useQuery();
+import { prisma } from '~/server/db';
+
+export const getServerSideProps = (async ({ req }) => {
+	const { userId } = getAuth(req);
+	if (!userId) return { props: { role: null } };
+	const user = await prisma.user.findUnique({
+		where: { id: userId },
+		select: {
+			role: true,
+		},
+	});
+	return { props: { role: user?.role ?? null } };
+}) satisfies GetServerSideProps<{ role: Role | null }>;
+
+const AdminRouter = ({
+	role,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
 	const router = useRouter();
 	const { path } = router.query;
 	useEffect(() => {
